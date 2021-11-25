147f0a5d7519ae08985e1a060af53dddf9c922f8
Milind Jamnekar <milindjamnekar502@gmail.com>
2021-11-25 10:18:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Typography variant="h1" component="div">
+      Hello world
+    </Typography>
+  );
+}
+
+export default App;
